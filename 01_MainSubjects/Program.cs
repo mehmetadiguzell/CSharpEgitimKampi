1244ed2fa0a4991a40f72dec2ebfd1e9334dcf16
Mehmet Adıgüzel <adiguzelmehmet1997@gmail.com>
2025-02-11 14:33:10 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,92 @@
+﻿
+#region Yazdırma Komutları
+/*
+Console.WriteLine("Selam");
+Console.Write("Merhaba Dünya");
+
+Console.WriteLine("***** Yemek Kategorileri *****");
+Console.WriteLine();
+Console.WriteLine("1-Çorbalar");
+Console.WriteLine("2-Ana Yemekler");
+Console.WriteLine("3-Soğuk Başlangıçlar");
+Console.WriteLine("4-Salatalar");
+Console.WriteLine("5-Tatlılar");
+Console.WriteLine("6-İçecekler");
+Console.WriteLine();
+Console.WriteLine("***** Yemek Kategorileri *****");
+*/
+#endregion
+
+#region Degiskenler
+
+//string
+//degisken_turu degisken_adi;
+
+//string name;
+//name = "Mehmet";
+//Console.WriteLine(name);
+
+//string customerName;
+//string customerSurname;
+//string customerPhone;
+//string customerEmail,district,city;
+
+//customerName = "Mustafa";
+//customerSurname = "Toprak";
+//customerPhone = "11 111 11 11";
+//customerEmail = "deneme@gmail.com";
+//district = "Kadıköy";
+//city = "İstanbul";
+
+//Console.WriteLine("*** Rezervasyon Kartı ****");
+//Console.WriteLine();
+//Console.WriteLine("------------------------");
+//Console.WriteLine("Müşteri:" + customerName + " " + customerSurname);
+//Console.WriteLine("İletişim:"+ customerPhone);
+//Console.WriteLine("Email"+ customerEmail);
+//Console.WriteLine("Adres"+ district + "/" + city);
+//Console.WriteLine("------------------------");
+#endregion
+
+#region Int Degiskenler
+/*
+int number = 24;
+Console.WriteLine(number);
+
+int cokePrice = 35;
+int waterPrice = 10;
+int pizzaPrice = 300;
+
+Console.WriteLine("**** Restoran Menü Fiyati ****");
+Console.WriteLine();
+Console.WriteLine("---- Kola : " + cokePrice + "Tl");
+Console.WriteLine("---- Su : " + waterPrice + "Tl");
+Console.WriteLine("---- Pizza : " + pizzaPrice + "Tl");
+Console.WriteLine();
+Console.WriteLine("**** Restoran Menü Fiyati ****");
+
+Console.WriteLine();
+
+int totalCokePrice = 0;
+int totalWaterPrice = 0;
+int totalPizzaPrice = 0;
+
+int cokeCount = 3;
+int waterCount = 2;
+int pizzaCount = 1;
+
+totalCokePrice = cokeCount * cokePrice;
+totalWaterPrice = waterCount * waterPrice;
+totalPizzaPrice=totalPizzaPrice * pizzaPrice;
+
+Console.WriteLine("--------------------------------------");
+
+Console.WriteLine("Cola Tutarı: " + totalCokePrice + "TL");
+Console.WriteLine("Su Tutatarı: " + totalWaterPrice + "TL");
+Console.WriteLine("Pizza Tutatarı: " + totalPizzaPrice + "TL");
+
+int totalPrice = totalCokePrice + totalPizzaPrice + totalWaterPrice;
+Console.WriteLine("Toplam Tutar : " + totalPrice +"TL");
+
+*/
+#endregion
